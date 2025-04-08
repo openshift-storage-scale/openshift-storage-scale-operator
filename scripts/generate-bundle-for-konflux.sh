e39965c95ce6d22b32569a664101860f55abfec9
Jordi Gil <jgil@redhat.com>
2025-04-07 21:00:00 -0400

    Move operator-sdk stage in bundle.konflux.Dockerfile to the generate-bundle-for-konflux script to group the logic that generates the bundle information and also to simplify the containerfile
    Signed-off-by: Jordi Gil <jgil@redhat.com>

---
+++ scripts/generate-bundle-for-konflux.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Source the environment values that contain the component image pullspecs coming from the  nudges
+for file in nudges/*.env
+do . $file
+done
+
+# Run the bundle generation flow, including replacing the image pullspecs in the config templates
+# to the ones from the nudges with the 'envsubst' command
+kustomize build config/manifests/ | \
+envsubst | \
+operator-sdk generate bundle \
+        -q \
+        --overwrite \
+        --version $(cat VERSION.txt) \
+        --output-dir bundle \
+        --channels development \
+        --default-channel development \
+        --use-image-digests
+operator-sdk bundle validate ./bundle
+
+# Generate the bundle.konflux.Dockerfile with the label values for the components that
+# are used as references in the bundle replaced by the actual values from the env values
+# using the 'envsubst' command.
+# These labels are used in the release process to validate that the bundle related image pullspecs
+# match with the Snapshot component pullspecs.
+cat templates/bundle.konflux.Dockerfile.template | envsubst >bundle.konflux.Dockerfile
--- scripts/generate-bundle-konflux-containerfile.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-for file in nudges/*.env
-do . $file
-done
-
-cat templates/bundle.konflux.Dockerfile.template | envsubst >bundle.konflux.Dockerfile
-
