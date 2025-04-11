7ac36175b0b8c6cae882eaf08f173ca8a1a1bfef
Michele Baldessari <michele@acksyn.org>
2025-04-11 20:22:37 +0200

    Also build and push must-gather when releasing

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,7 +5,8 @@ VERSION=$(cat VERSION.txt)
 
 make USE_IMAGE_DIGESTS="" bundle generate manifests docker-build docker-push \
     bundle-build bundle-push catalog-build catalog-push \
-    console-build console-push devicefinder-docker-build devicefinder-docker-push
+    console-build console-push devicefinder-docker-build devicefinder-docker-push \
+    must-gather-docker-build must-gather-docker-push
 
 podman tag quay.io/openshift-storage-scale/openshift-fusion-access-catalog:${VERSION} quay.io/openshift-storage-scale/openshift-fusion-access-catalog:stable
 
