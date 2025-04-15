9911b7d2a390a6ea246d6048ead1f81aaa7d08bb
Jordi Gil <jgil@redhat.com>
2025-04-15 13:20:04 -0400

    Use ADDITIONAL_SECRET param in build task to inject secret
    Signed-off-by: Jordi Gil <jgil@redhat.com>

--- hack/build.sh
+++ hack/build.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -ex
 
-# Placeholder env variable for the name of the secret defined in the build-container task in Konflux.
-# Konflux will mount the secret in /run/secrets/{secret_name}/{contents_of_the_secret}, but
-# our dev environment doesn't have that {secret_name} and it should be agnostic to it.
-# Hence empty by default
-SECRET_NAME=
 # GOOS and GOARCH will be set if calling from make. Dockerfile calls this script
 # directly without calling make so the default values need to be set here also.
 [[ -z "$GOOS" ]] && GOOS=linux
@@ -32,8 +27,9 @@ esac
 go version
 
 touch internal/controller/pull.txt
-if [ -f "/run/secrets/pull" ]; then
-    cp -f /run/secrets/pull internal/controller/pull.txt
+if [ -f "/run/secrets/{SECRET_NAME}/pull" ]; then
+    echo "Secret found in /run/secrets/{SECRET_NAME}/pull"
+    cp -f /run/secrets/{SECRET_NAME}/pull internal/controller/pull.txt
 fi
 
 GOFLAGS=-mod=vendor CGO_ENABLED=0 GOOS=$GOOS GOARCH=$GOARCH go $EXTRA -ldflags="${LDFLAGS}" cmd/main.go
