4287535302c66ce14b9050aa68baf5be04518388
Jordi Gil <jgil@redhat.com>
2025-04-16 20:19:24 -0400

    Fix bundle pipeline issues where the dockerfile was still using the old bundle.konflux.Dockerfile name instead of bundle.Dockerfile
    Signed-off-by: Jordi Gil <jgil@redhat.com>

--- scripts/generate-bundle-for-konflux.sh
+++ scripts/generate-bundle-for-konflux.sh
@@ -6,7 +6,7 @@ do $(cat $file | tail -n+2 )
 done
 
 # Expose the operator's version as an env variable to be used when building the operator-sdk's bundle
-# manifests and also in the bundle.konflux.Dockerfile's label
+# manifests and also in the bundle.Dockerfile's label
 export VERSION=$(cat VERSION.txt)
 
 # Run the bundle generation flow, including replacing the image pullspecs in the config templates
@@ -23,9 +23,9 @@ operator-sdk generate bundle \
         --use-image-digests
 operator-sdk bundle validate ./bundle
 
-# Generate the bundle.konflux.Dockerfile with the label values for the components that
+# Generate the bundle.Dockerfile with the label values for the components that
 # are used as references in the bundle replaced by the actual values from the env values
 # using the 'envsubst' command.
 # These labels are used in the release process to validate that the bundle related image pullspecs
 # match with the Snapshot component pullspecs.
- envsubst < templates/bundle.konflux.Dockerfile.template >bundle.konflux.Dockerfile
+ envsubst < templates/bundle.Dockerfile.template >bundle.Dockerfile
