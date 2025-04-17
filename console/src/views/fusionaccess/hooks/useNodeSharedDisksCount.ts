3ec8af1c20475b336bfae6cfa1104514dd1c9c73
Jonathan Kilzi <jkilzi@redhat.com>
2025-04-17 08:59:08 +0300

    Simplify useNodeSharedDisksCount.ts
    Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>

--- console/src/views/fusionaccess/hooks/useNodeSharedDisksCount.ts
+++ console/src/views/fusionaccess/hooks/useNodeSharedDisksCount.ts
@@ -1,8 +1,5 @@
 import { useMemo } from "react";
-import type {
-  DiscoveredDevice,
-  LocalVolumeDiscoveryResult,
-} from "@/models/fusionstorage/LocalVolumeDiscoveryResult";
+import type { LocalVolumeDiscoveryResult } from "@/models/fusionstorage/LocalVolumeDiscoveryResult";
 import type { IoK8sApiCoreV1Node } from "@/models/kubernetes/1.30/types";
 
 export const useNodeSharedDisksCount = (
@@ -16,69 +13,43 @@ export const useNodeSharedDisksCount = (
       return 0;
     }
 
-    // Filter out discovery results that don't belong to the selected nodes
-    const discoveryResultsForSelectedNodes = discoveryResults.filter((result) =>
+    // Only consider results from selected nodes
+    const selectedDiscoveryResults = discoveryResults.filter((result) =>
       selectedNodes.some((node) => node.metadata?.name === result.spec.nodeName)
     );
 
-    // If selected nodes have no discovery results, return 0
-    if (discoveryResultsForSelectedNodes.length === 0) {
+    if (selectedDiscoveryResults.length === 0) {
       return 0;
     }
 
-    // Get the WWNs of the given node
+    // Get the WWNs of the input node
+    const nodeDiscoveryResult = discoveryResults.find(
+      (result) => result.spec.nodeName === nodeName
+    );
     const nodeWwns = new Set(
-      discoveryResults
-        .find((result) => result.spec.nodeName === nodeName)
-        ?.status.discoveredDevices?.map(({ WWN }) => WWN) ?? []
+      nodeDiscoveryResult?.status.discoveredDevices?.map(({ WWN }) => WWN) ?? []
     );
 
-    // If the given node has no WWNs, return 0
     if (nodeWwns.size === 0) {
       return 0;
     }
 
-    // Simplify the argument structure by mapping node names to a Set of their discovered devices WWNs
-    const selectedNodeNamesToDiscoveredDeviceWwnsMap =
-      discoveryResultsForSelectedNodes.reduce(
-        (map, item) => {
-          const nodeName = item.spec.nodeName; // Likely safe to assume nodeName is unique...
-          const nodeDiscoveredDevicesWwns = new Set(
-            item.status.discoveredDevices?.map(({ WWN }) => WWN) ?? []
-          );
+    // Gather WWNs for all other selected nodes
+    const otherNodesWwns = new Set<string>();
+    selectedDiscoveryResults.forEach((result) => {
+      if (result.spec.nodeName !== nodeName) {
+        result.status.discoveredDevices?.forEach(({ WWN }) => {
+          otherNodesWwns.add(WWN);
+        });
+      }
+    });
 
-          if (map.has(nodeName)) {
-            // ...but just in case.
-            map.set(
-              nodeName,
-              map.get(nodeName)!.union(nodeDiscoveredDevicesWwns)
-            );
-          } else {
-            map.set(nodeName, nodeDiscoveredDevicesWwns);
-          }
-
-          return map;
-        },
-        new Map() as Map<string, Set<DiscoveredDevice["WWN"]>>
-      );
-
-    // Gather all WWNs toghether but exclude those in the given node
-    const allOtherSelectedNodesDiscoveredDevicesWwns =
-      selectedNodeNamesToDiscoveredDeviceWwnsMap
-        .entries()
-        .reduce(
-          (set, [name, wnns]) => (name !== nodeName ? set.union(wnns) : set),
-          new Set<DiscoveredDevice["WWN"]>()
-        );
-
-    // If no other selected nodes are found, return 0
-    if (allOtherSelectedNodesDiscoveredDevicesWwns.size === 0) {
+    if (otherNodesWwns.size === 0) {
       return 0;
     }
 
-    // Count the number of shared disks in the given node with all other nodes
-    const sharedDisksWwns =
-      allOtherSelectedNodesDiscoveredDevicesWwns.intersection(nodeWwns);
+    // Count the number of shared disks
+    const sharedDisksWwns = nodeWwns.intersection(otherNodesWwns);
 
     return sharedDisksWwns.size;
   }, [isSelected, selectedNodes, discoveryResults, nodeName]);
