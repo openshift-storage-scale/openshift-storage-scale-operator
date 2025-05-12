e54abe035e97f77e8ea1086d5a70953ed1ebe62a
rawagner <rawagner@redhat.com>
2025-05-12 16:15:21 +0200

    Filter out non-shared disks when creating FileSystem update name regex

--- console/src/utils/fusion-access/LocalVolumeDiscoveryResult.ts
+++ console/src/utils/fusion-access/LocalVolumeDiscoveryResult.ts
@@ -2,5 +2,5 @@ import type { DiscoveredDevice } from "@/models/fusion-access/LocalVolumeDiscove
 
 const WWN_PREFIX_LENGTH = "uuid.".length;
 
-export const getShortWwn = (device: DiscoveredDevice, length = 8) =>
-  device.WWN.slice(WWN_PREFIX_LENGTH, WWN_PREFIX_LENGTH + length);
+export const getWwn = (device: DiscoveredDevice) =>
+  device.WWN.slice(WWN_PREFIX_LENGTH);
